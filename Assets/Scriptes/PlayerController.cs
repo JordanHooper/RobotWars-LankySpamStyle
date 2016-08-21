91c532c0cf5329c6b0f9b03919c6602872193f69
JordanHooper <jordan.hooper@hotmail.co.uk>
2016-08-21 21:20:56 +0100

    v 0.1
    - Arena and basic bot shape added
    - basic movement scripts (need work)
    - Added triggers for flipper and pit release button

---
+++ Assets/Scriptes/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 12f;
+
+    Vector3 movement;
+    public Rigidbody botRigidBody;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            botRigidBody.velocity = new Vector3(0f, 0f, 10f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 1.5f, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -1.5f, 0);
+        }
+    }
+}
