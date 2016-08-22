b60640921e2f3a4c92fccef59797503983640836
JordanHooper <jordan.hooper@hotmail.co.uk>
2016-08-22 23:21:14 +0100

    v 0.1.3
    - Added Textures
    - Started work on flipper script (needs work)
    - Resized Player
    - Fixed Overlapping piece issues

---
+++ Assets/Scriptes/Flipper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flipper : MonoBehaviour
+{
+    public Transform flipper;
+
+    void Awake()
+    {
+        //flipper = GetComponentInParent<Transform>();
+    }
+
+    void OnTriggerEnter()
+    {
+        //Flipper.transform.rotation(-40, 0, -40);
+    }
+
+}
