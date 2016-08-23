be39ecb4434a2df2914d8f83c70d13b9ae73e5eb
JordanHooper <jordan.hooper@hotmail.co.uk>
2016-08-23 13:45:06 +0100

    v 0.1.4
    - Updated bot to look more realistic
    - Bot now needs movement fixes
    - Wheel movement script added (needs work)
    - Flipper problems, needs collider as you fall straight through at
    present + through the flipper ground

---
+++ Assets/Scriptes/Wheel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wheel : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            this.transform.Rotate(0, -(Time.deltaTime * 50) - 10, 0);           //	roatate the wheel
+        }
+        if (Input.GetKey("s"))
+        {
+            this.transform.Rotate(0, (Time.deltaTime * 25) + 5, 0);             //rotate the wheel
+        }
+    }
+}
